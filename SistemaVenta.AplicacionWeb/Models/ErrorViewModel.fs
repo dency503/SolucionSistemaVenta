f5982867c0a1aaf9041ac4c19445519e58b64985
Dency503 <dennisgodinez64@gmail.com>
2024-05-01 19:56:04 -0500

    first commit

---
+++ SistemaVenta.AplicacionWeb/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace SistemaVenta.AplicacionWeb.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
---
+++ SistemaVenta.DAL/Implementacion/VentaRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.DAL.Implementacion
+{
+    public class VentaRepository : GenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbcontext;
+        public VentaRepository(DbventaContext dbcontext) : base(dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<Venta> Registrar(Venta entidad)
+        {
+            Venta ventaGenerada = new Venta();
+            using(var transaction = _dbcontext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach(DetalleVenta dv in entidad.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbcontext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _dbcontext.Productos.Update(producto_encontrado);
+                    }
+                    await _dbcontext.SaveChangesAsync();
+                    NumeroCorrelativo correlativo = _dbcontext.NumeroCorrelativos.Where(n => n.Gestion == "venta").First();
+                    
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;
+                    correlativo.FechaActualizacion = DateTime.Now;
+                    _dbcontext.NumeroCorrelativos.Update(correlativo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    string ceros = string.Concat(Enumerable.Repeat("0", correlativo.CantidadDigitos.Value));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - correlativo.CantidadDigitos.Value, correlativo.CantidadDigitos.Value);
+
+                    entidad.NumeroVenta = numeroVenta;
+                    await _dbcontext.Venta.AddAsync(entidad);
+                    await _dbcontext.SaveChangesAsync();
+
+                    ventaGenerada = entidad;
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+            return ventaGenerada;
+        }
+
+        public async Task<List<DetalleVenta>> Reporte(DateTime FechaInicio, DateTime FechaFin)
+        {
+            List<DetalleVenta> listaResumen = await _dbcontext.DetalleVenta
+                 .Include(v => v.IdVentaNavigation)
+                 .ThenInclude(u => u.IdUsuarioNavigation)
+                 .Include(v => v.IdVentaNavigation)
+                 .ThenInclude(tdv => tdv.IdTipoDocumentoVentaNavigation)
+                 .Where(dv => dv.IdVentaNavigation.FechaRegistro.Value.Date >= FechaInicio.Date &&
+                  dv.IdVentaNavigation.FechaRegistro.Value.Date <= FechaFin.Date).ToListAsync();
+            
+            return listaResumen;
+        }
+    }
+}
